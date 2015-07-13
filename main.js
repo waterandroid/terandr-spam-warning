fc0040d9e6d8a01b0180f4f9dd0c685c11ab828c
waterandroid <waterandroid@users.noreply.github.com>
2015-07-13 17:40:20 -0400

    Removed Specific Info
    Linked to settings.js and settings_dev.js
    Added empty command: viewspam
    Added unusued function to reduce count of messages

--- main.js
+++ main.js
@@ -1,94 +1,80 @@
 var bot = require("../../terandr");
+var fs = require("fs");
+var settings = require("./settings");
+if (fs.existsSync("settings_dev.js")) {
+	settings = require("./settings_dev");
+}
+var moderationChannels = settings.moderationChannels;
+var mainChannel = settings.mainChannel;
 
 module.exports = {
   functions: {
     parseText: function(sender, channel, text) {
       var textSplit = text.indexOf(":");
-      var steamUser = text.substr(0, textSplit);
+      var player = text.substr(0, textSplit);
       var message = text.substr(textSplit + 2);
       var numMessages = playerMessagesList.length;
       for (i = 0; i < numMessages; i++) {
-        if ((playerMessagesList[i].player == steamUser) && playerMessagesList[i].chat == message) {
+        if ((playerMessagesList[i].player == player) && playerMessagesList[i].chat == message) {
           playerMessagesList[i].count++;
           if (playerMessagesList[i].count >= 4) {
             //Alert: Player Spamming
-            bot.bot.message(mainChannel, "AdminAlert: " + steamUser + " is spamming \"" + message + "\" on " + channel + ". Count: " + playerMessagesList[i].count);
+            bot.bot.message(mainChannel, "AdminAlert: " + player + " is spamming \"" + message + "\" on " + channel + ". Count: " + playerMessagesList[i].count);
           }
           return true;
         }
       }
-      playerMessagesList.push({player:steamUser, chat: message, channel: channel, count: 1});
+      playerMessagesList.push({player:player, chat: message, channel: channel, count: 1});
       return true;
     },
     enterChannels: function() {
       for (i = 0; i < moderationChannels.length; i++) {
-        //bot.bot.message("#waterandroid", "HI");
         bot.bot.join(moderationChannels[i]);
       }
-    }
+    },
+    reduceCount: function() {
+      if (playerMessagesList.length === 0) {
+        return false;
+      }
+      for (i = 0; i < playerMessagesList.length; i++) {
+        if (playerMessagesList <= 1) {
+          playerMessagesList.splice(i, 1);
+        }
+        playerMessagesList[i].count--;
+      }
+    },
   },
   onStart: [
     "enterChannels",
+    "reduceCount"
   ],
   onMessage: [
     "parseText"
+  ],
+  commands: [
+    {
+      viewspam: function(parameters) {
+        viewSpamList(parameters);
+      }
+    }
   ]
 };
 
-var botTracker = "waterandroid"; //change this to Firepowered when working
-var mainChannel = "#waterandroid";
+function viewSpamList(parameters) {
+
+}
+
 var playerMessagesList = [
   {
     player: "test",
-    chat: "Hi! I like binds!",
-    channel: "#FP-ABHT",
+    chat: "Hi! I like spam!",
+    channel: "#foo",
     count: 1
   },
   {
     player: "test2",
-    chat: "Hello! I love binds more!",
-    channel: "FP-ABHT",
+    chat: "Hello! I love spam more!",
+    channel: "#bar",
     count: 4
   },
-];
-
-function parseText(text) {
-  var textSplit = text.indexOf(":");
-  var steamUser = text.substr(1, textSplit - 1);
-  var message = text.substr(textSplit + 1);
-  var numMessages = playerMessagesList.length;
-  for (i = 0; i < numMessages; i++) {
-    if ((playerMessagesList[i].player == steamUser) && playerMessagesList[i].chat == message) {
-      playerMessagesList[i].count++;
-      if (playerMessagesList[i].count >= 4) {
-        //Alert: Player Spamming
-        bot.message(mainChannel, "AdminAlert: " + SteamUser + "is spamming \"" + message + "\" on " + channel);
-      }
-      return true;
-    }
-  }
-  playerMessagesList.push({player:steamUser, chat: message, channel: channel, count: 1});
-  return true;
-}
-
-var moderationChannels = [
-  /*  "#FP-AB2F",
-  "#FP-ABDB",
-  "#FP-ABDK",
-  "#FP-ABHT",
-  "#FP-ABHV",
-  "#FP-ABJP",
-  "#FP-ABMGE",
-  "#FP-ABPL",
-  "#FP-ABSURF",
-  "#FP-ABTR1",
-  "#FP-ABUN1",
-  "#FP-PHDB",
-  "#FP-PHFT",
-  "#FP-PHHV",
-  "#FP-PHJP",
-  "#FP-PHMGE",
-  "#FP-PHPL",
-  "#FP-PHTB",*/
-  "#waterandroid2"
 ];
